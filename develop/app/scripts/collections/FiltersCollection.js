61c64c2fd4e4474080a084a90e40426cce801765
Hon-Chih Chen <honchih@biglaker.com>
2015-10-08 14:45:35 -0400

    removed old scripts folder

--- develop/app/scripts/collections/FiltersCollection.js
+++
@@ -1,8 +0,0 @@
-var Backbone = require('backbone');
-
-var FiltersCollection = Backbone.Collection.extend({
-    model: NortonApp.Models.Filter,
-    url: '/json/filters.json',
-});
-
-module.exports = FiltersCollection;
