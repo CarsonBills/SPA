b2dd0edf61abd3cf46362c05188a42ac7a1c85c8
Dietrich, Peter <pdietrich@wwnorton.com>
2015-11-12 10:34:43 -0500

    Fix to read accept QS params

--- develop/app_nr/php/searchandiser.php
+++ develop/app_nr/php/searchandiser.php
@@ -1,19 +1,43 @@
 <?php
+
+if (!isset($_REQUEST['sort'])) {
+    //var_dump($_REQUEST);exit;
+}
+
+
+$platform = "Prod";
+$area = "Prod";
+
 $query = (isset($_REQUEST['query']) && $_REQUEST['query']) ? ',"query"' . ': ' . '"'.$_REQUEST['query'].'"' : "";
 
-$skip = (isset($_REQUEST['skip']) && (int)$_REQUEST['skip'] > 0) ? (int)$_REQUEST['skip'] : 0;
-$pageSize = (isset($_REQUEST['pageSize']) && (int)$_REQUEST['pageSize'] > 0) ? (int)$_REQUEST['pageSize'] : 10;
+$skip = (isset($_REQUEST['skip']) && (int)$_REQUEST['skip'] > 0) ? ',"skip":' . (int)$_REQUEST['skip'] : ',"skip": 0';
+$sitecode = (isset($_REQUEST['sitecode']) && $_REQUEST['sitecode']) ? $_REQUEST['sitecode'] : "nortonreader";
+$siteversion = (isset($_REQUEST['siteversion']) && $_REQUEST['siteversion']) ? $_REQUEST['siteversion'] : "full";
+$pageSize = (isset($_REQUEST['pageSize']) && (int)$_REQUEST['pageSize'] > 0) ? ',"pageSize":' . (int)$_REQUEST['pageSize'] . '}'  : ',"pageSize": 10}';
 $filters = (isset($_REQUEST['refinements']) && $_REQUEST['refinements']) ? ',"refinements": ' . $_REQUEST['refinements'] : "";
 
-    $url = "https://wwnorton.groupbycloud.com/api/v1/search?pretty";
+if (isset($_REQUEST['sort'])) {
+    $field = $_REQUEST['sort']['field'];
+    $order = $_REQUEST['sort']['order'];
+    $sort = ',"sort": { "field": "'.$field.'", "order": "'.$order.'" }';
+} else {
+    $sort = "";
+}
+if (isset($_REQUEST['fields'])) {
+    $fields = ',"fields":["'.$_REQUEST['fields'][0].'"] ';
+} else {
+    $fields = ',"fields":["*"] ';
+}
 
-     $data='{"clientKey":"8fd00c47-8378-455c-a1bc-e4f1f1704b87","collection":"nortonreader","area":"Production"' .
-        $query .
-        ',"fields":["*"] ' .
-        $filters .
-        ',"skip":' . $skip .
-        ',"pageSize":' . $pageSize . '}';
+$url = "https://wwnorton.groupbycloud.com/api/v1/search?pretty";
 
+$data='{"clientKey":"8fd00c47-8378-455c-a1bc-e4f1f1704b87","collection":"' . $sitecode.$platform . '","area":"'.$area.'"' .
+    $query .
+    $fields .
+    $sort .
+    $filters .
+    $skip .
+    $pageSize;
 
 //echo $data;exit;
 $h=curl_init();
