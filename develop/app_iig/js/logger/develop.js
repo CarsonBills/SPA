a5b6cfed8065f9a04dacb17060fa4f032c653acd
Hon-Chih Chen <honchih@biglaker.com>
2015-11-25 23:15:50 -0500

    dynamic logger insertion

---
+++ develop/app_iig/js/logger/develop.js
@@ -0,0 +1,9 @@
+Logger = require('js-logger');
+Logger.useDefaults();
+//Logger.setLevel(Logger.OFF);
+
+Logger.setHandler(function (messages, context) {
+    // Send messages to a custom logging endpoint for analysis.
+    //jQuery.post('/logs', { message: messages[0], level: context.level });
+    console.log(context.level, messages[0])
+});
