a86068befa61f7519913ac875dab75d5aaa45b24
Hon-Chih Chen <honchih@biglaker.com>
2015-11-26 11:02:31 -0500

    tweaked logger

--- develop/app_iig/js/logger/production.js
+++ develop/app_iig/js/logger/production.js
@@ -1,6 +1,6 @@
 Logger = require('js-logger');
 Logger.useDefaults();
-Logger.setLevel(Logger.OFF);
+//Logger.setLevel(Logger.OFF);
 
 Logger.setHandler(function (messages, context) {
     // Send messages to a custom logging endpoint for analysis.
