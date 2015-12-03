9fdf555fb6ac816d894efe03168b81ddb29de954
Hon-Chih Chen <honchih@biglaker.com>
2015-12-03 15:43:18 -0500

    qa2 constant

---
+++ develop/app_iig/js/modules/js_qa_constants.js
@@ -0,0 +1,21 @@
+var JSConstants = {
+    noMyItems: '<div class="empty-list">You have not added any items to your List yet.</div>',
+    emptySeach: 'Please put in search query',
+    creditsUrl: '/json/credits.json',
+    loginUrl: '//books.wwnorton.com/books/ssl/WebLogin.aspx',
+    invalidSiteCodeUrl: '//books.wwnorton.com/books/index.aspx',
+    siteConfigUrl: '//qa2-services.wwnorton.com/getsiteconfig.php?',
+    searchUrl: '//qa2-services.wwnorton.com/search.php',
+    getDetailPageUrl: '//qa2-services.wwnorton.com/getpagedetail.php?',
+    awsContentUrl: '//qa2-services.wwnorton.com/get_content.php?',
+    saveTrackingUrl: '//qa2-services.wwnorton.com/tracking.php',
+    likeUnlikeAssetUrl: '//qa2-services.wwnorton.com/like_unlike.php',
+    getSavedFavsUrl: '//qa2-services.wwnorton.com/get_favorites.php',
+    tabletLandspaceWidth: 1024,
+    tabletPortaitWidth: 768,
+    mobileWidth: 480,
+    mobileWidthSM: 320,
+    defaultPageSize: 10
+};
+
+module.exports = JSConstants;
