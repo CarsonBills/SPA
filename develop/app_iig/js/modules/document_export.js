1aadd725f19070b0163203fdf85feec088b1cdff
Dietrich, Peter <pdietrich@wwnorton.com>
2015-12-03 14:23:04 -0500

    new helper files

---
+++ develop/app_iig/js/modules/document_export.js
@@ -0,0 +1,33 @@
+var Backbone = require('backbone'),
+    _ = require('underscore'),
+
+    DocumentExport = (function() {
+        'use strict';
+        var getContent = function (params) {
+
+            var separator,
+                type = params.type,
+                data = params.title + '\n';
+
+            if (type === 'csv') {
+                separator = ', ';
+            } else if (type === 'rtf') {
+                separator = ' ';
+            }
+            data += (params.legend.join(separator) + '\n');
+
+            _.each(params.collection.models, function(article) {
+                data += article.attributes.title + separator +
+                    article.attributes.authorFirstName + ' ' + article.attributes.authorLastName + separator +
+                    ' page ' + article.attributes.pageNumber + '\n';
+            });
+
+            return 'data:application/' + type + ';charset=utf-8,' + encodeURIComponent(data);
+        };
+
+        return {
+            getContent: getContent
+        };
+    }());
+
+module.exports = DocumentExport;
