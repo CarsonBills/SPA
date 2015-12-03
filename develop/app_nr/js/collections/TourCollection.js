8ace7c0498d2dd94d55a255de954f3b1bbf5036b
Hon-Chih Chen <honchih@biglaker.com>
2015-12-03 10:17:19 -0500

    tour tweaks

--- develop/app_nr/js/collections/TourCollection.js
+++ develop/app_nr/js/collections/TourCollection.js
@@ -9,26 +9,31 @@ var TourModel = Backbone.Collection.extend({
         this.set([{
                 anchor: '#searchButton',
                 content: 'Search for a reading',
-                container: '.search',
+                container: '.container',
+                placement: 'auto bottom'
+            }, {
+                anchor: '#sortArticles',
+                content: 'Sort the readings alphabetically by author or title',
+                container: '.container',
                 placement: 'auto bottom'
             }, {
                 anchor: '#navYourFavs',
-                content: 'Everything you save gets stored in your list here',
-                container: '.search',
+                content: 'All of the reading you save are stored in a list that you can edit and download here',
+                container: '.container',
                 placement: 'auto bottom'
             }, {
                 anchor: 'li:nth-child(4) .savelist-lnk',
-                content: 'Add a reading to your syllabus for later',
+                content: 'Add a reading to your list of saved items',
                 container: '.container',
                 placement: 'auto bottom'
             }, {
                 anchor: '.filter-item.collapse.in .filter-item-name:nth-child(3)',
-                content: 'Browse the book by selecting one or more of these categories.',
-                container: '#filtersREAL',
+                content: 'Browse the book by selecting one or more of the readings in these categories.',
+                container: '.container',
                 placement: 'auto left'
             }, {
                 anchor: 'li:nth-child(9) .ebook',
-                content: 'View the reading in the ebook.',
+                content: 'View a reading in the ebook (sign in required)',
                 container: '.container',
                 placement: 'auto bottom'
             }]);
