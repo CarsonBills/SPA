89339745f68364551bf2912140003f6570cdc981
Hon Chih Chen <hchen@wwnorton.com>
2016-02-05 09:55:15 -0500

    fixed typo

--- develop/app_iig/js/dynamic/constants_dev.js
+++ develop/app_iig/js/dynamic/constants_dev.js
@@ -1,6 +1,6 @@
 var JSConstants = {
     noMyItems: '<div class="empty-list">You have not added any items to your List yet.</div>',
-    emptySeach: 'Please put in search query',
+    emptySearch: 'Please put in search query',
     creditsUrl: '/json/credits.json',
     loginUrl: '//books.wwnorton.com/books/ssl/WebLogin.aspx',
     invalidSiteCodeUrl: '//books.wwnorton.com/books/index.aspx',
